3c92ad677213ae82a7c476b431e49c1fd1f68220
Yuri Almeida <yurialves2@gmail.com>
2021-08-25 22:31:37 -0300

    add class methods and constructor

--- src/dartbank/bin/main.dart
+++ src/dartbank/bin/main.dart
@@ -1,6 +1,17 @@
 import 'package:dartbank/BankAccount.dart';
 
-void main(List<String> arguments) {
-  print(BankAccount('John', 1, 1));
-}
+void main (List<String> arguments) {
+  var johnAccount = BankAccount('John', 001, 1);
+  var johnAccountClone = BankAccount('John', 001, 1);
+  var janeAccount = BankAccount('Jane', 001, 2, balance: 10.0); // overriding default balance 
 
+  print(johnAccount == johnAccountClone);
+  print('HashCodes: ${johnAccount.hashCode} ${johnAccountClone.hashCode}');
+
+  johnAccount.deposit(100);
+  johnAccount.withdraw(50);
+  johnAccount.transfer(15, janeAccount);
+
+  print(johnAccount);
+  print(janeAccount);
+}
--- src/dartbank/lib/BankAccount.dart
+++ src/dartbank/lib/BankAccount.dart
@@ -3,4 +3,40 @@ class BankAccount {
   late int agency;
   late int accountNumber;
   late double balance = 0.0;
+
+  bool get isBalancePositive => balance > 0;
+
+  BankAccount(
+    String owner,
+    int agency,
+    int accountNumber,
+    { double balance = 0.0 } // default value / optional initialization with named argument
+  ) {
+    this.owner = owner;
+    this.agency = agency;
+    this.balance = balance;
+    this.accountNumber = accountNumber;
+  }
+
+  void deposit(double amount) {
+    balance += amount;
+  }
+
+  double withdraw(double amount) {
+    if (amount > balance) return 0;
+    balance -= amount;
+    return amount;
+  }
+
+  void transfer(double amount, BankAccount destination) {
+    if (amount > balance) {
+      throw Exception('Insuficient balance');
+    }
+    destination.deposit(withdraw(amount));
+  }
+
+  @override
+  String toString() {
+    return '{owner: $owner, balance: $balance}';
+  }
 }
