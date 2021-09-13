78b1efb1c7e34325cc5cccc9da8fe0ecc860e138
Yuri Almeida <yurialves2@gmail.com>
2021-09-13 02:28:28 -0300

    syntatic sugar assigment to members

--- src/dartbank/lib/Client.dart
+++ src/dartbank/lib/Client.dart
@@ -1,11 +1,19 @@
 class Client {
-  late String name;
-  late String document;
+  String name;
+  String document;
 
-  Client(String name, String document) {
-    this.name = name;
-    this.document = document;
-  } 
+  // syntactic sugar for assignment to members
+  Client(this.name, this.document);
 
   @override String toString() => name;
+}
+
+void main() {
+  // cascade notation
+  var client = Client('foo', 'bar')
+    ..name = 'Foo'
+    ..document = 'Bar';
+
+  print(client.name);
+  print(client.document);
 }
