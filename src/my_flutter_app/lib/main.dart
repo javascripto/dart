52867b1931ab443ea0c62e6284e632e716aea7a7
Yuri Almeida <yurialves2@gmail.com>
2021-09-13 22:33:01 -0300

    chore: added transferences page

--- src/my_flutter_app/lib/main.dart
+++ src/my_flutter_app/lib/main.dart
@@ -1,8 +1,52 @@
 
 import 'package:flutter/material.dart';
-import 'package:my_flutter_app/counter.dart';
-
 
 void main() {
-  runApp(Counter());
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Transferências'),
+        ),
+        body: Column(
+          children: [
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('100.0'),
+                subtitle: Text('1000'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('100.0'),
+                subtitle: Text('1000'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('100.0'),
+                subtitle: Text('1000'),
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
 }
