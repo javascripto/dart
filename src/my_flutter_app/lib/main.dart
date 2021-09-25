0ad0a28b37d4972bde8a8e3f82435a1b68f1936c
Yuri Almeida <yurialves2@gmail.com>
2021-09-25 15:54:14 -0300

    separate componente into many

--- src/my_flutter_app/lib/main.dart
+++ src/my_flutter_app/lib/main.dart
@@ -1,4 +1,5 @@
 
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,35 +18,48 @@ class App extends StatelessWidget {
         appBar: AppBar(
           title: Text('Transferências'),
         ),
-        body: Column(
-          children: [
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('100.0'),
-                subtitle: Text('1000'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('100.0'),
-                subtitle: Text('1000'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('100.0'),
-                subtitle: Text('1000'),
-              ),
-            ),
-          ],
-        ),
+        body: TransferenceList(),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: () {},
         ),
+      ),
+    );
+  }
+}
+
+class TransferenceList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TransferenceItem(amount: '100.0', account: '1000'),
+        TransferenceItem(amount: '200.0', account: '1000'),
+        TransferenceItem(amount: '400.0', account: '1000'),
+        TransferenceItem(amount: '700.0', account: '1000'),
+      ] + List.generate(4, (index) => (
+        TransferenceItem(
+          amount: (Random().nextInt(100000) / 100).toStringAsFixed(2),
+          account: '1000',
+        )
+      )),
+    );
+  }
+}
+
+class TransferenceItem extends StatelessWidget {
+  final String amount;
+  final String account;
+
+  TransferenceItem({ required this.amount, required this.account });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(amount),
+        subtitle: Text(account),
       ),
     );
   }
