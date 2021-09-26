d3a83a483b0d0f815c1a07104f048e432c928673
Yuri Almeida <yurialves2@gmail.com>
2021-09-26 16:14:59 -0300

    fix: terminal problems

--- src/my_flutter_app/lib/hello.dart
+++ src/my_flutter_app/lib/hello.dart
@@ -1,7 +1,7 @@
 
 import 'package:flutter/material.dart';
 
-Widget HelloWorld() => Center(
+Widget helloWorld() => Center(
   child: Text(
     'Hello, world!',
     textDirection: TextDirection.ltr,
